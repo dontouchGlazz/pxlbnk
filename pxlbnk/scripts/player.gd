aa866892728186b60420153e6da830ee9024fa03
dontouchGlazz <grace.qx.yin@gmail.com>
2025-11-19 08:05:15 -0500

    updated player.gd
    it's only like 4 lines of code (literally) but i've finally connected desktop github and the hackatime project to pxlbnk so i'm gonna save it here

--- pxlbnk/scripts/player.gd
+++ pxlbnk/scripts/player.gd
@@ -1,4 +1,11 @@
 extends CharacterBody2D
 
 #input inputmap somehow here
-#changes to player.tcsn will also be reflected in the main.tcsn
+#changes to player.tcsn will also 
+#be reflected in the main.tcsn
+
+func get_input():
+	pass
+
+func _physics_process(_delta: float) -> void:
+	move_and_slide()
